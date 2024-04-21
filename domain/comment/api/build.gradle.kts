c989b08dc2fc5865940f1516644201be1bc59210
qumn <2476573497@qq.com>
2024-04-21 18:59:27 +0800

    fix(gradle): duplicate jar name

--- domain/comment/api/build.gradle.kts
+++ domain/comment/api/build.gradle.kts
@@ -1,6 +1,10 @@
+import org.jetbrains.kotlin.gradle.plugin.mpp.pm20.util.archivesName
+
 plugins {
     id("io.github.qumn.kotlin-library-conventions")
 }
+
+project.archivesName.set("comment-api")
 
 dependencies {
     implementation(project(":framework:exception"))
--- domain/system/api/build.gradle.kts
+++ domain/system/api/build.gradle.kts
@@ -1,6 +1,10 @@
+import org.jetbrains.kotlin.gradle.plugin.mpp.pm20.util.archivesName
+
 plugins {
     id("io.github.qumn.kotlin-library-conventions")
 }
+
+project.archivesName.set("system-api")
 
 dependencies {
     implementation(project(":framework:test"))
